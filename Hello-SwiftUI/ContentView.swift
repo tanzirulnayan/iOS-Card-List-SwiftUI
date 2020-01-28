1a66833ed3434e064883d0ae31ab8225032e4747
Tanzirul Haque Nayan <tanzirulhaque7@gmail.com>
2020-01-28 13:01:24 +0600

    Initial Commit

---
+++ Hello-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hello-SwiftUI
+//
+//  Created by Tanzirul Haque Nayan on 28/1/20.
+//  Copyright © 2020 Tanzirul Haque Nayan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
